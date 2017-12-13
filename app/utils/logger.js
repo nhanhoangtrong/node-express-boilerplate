77337dd003510d22935413edc0a60414d7e69f8b
Nhan Hoang <siege250294@gmail.com>
2017-12-13 11:57:03 +0700

    more verbose logger generator

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -2,45 +2,58 @@ const { createLogger, transports, format } = require('winston');
 const config = require('../config');
 const { combine, timestamp, label, prettyPrint, simple, splat } = format;
 
-const loggerTransports = config.get('logger:transports').map(({ type, level, filename }) => {
-    if (type === 'file') {
-        return new transports.File({
-            level,
-            filename: filename.replace('[timestamp]', Date.now().toString()),
-        });
-    } else {
-        return new transports.Console({
-            level,
-        });
-    }
-});
-
-const loggerFormats = {
-    combined: combine(
-        timestamp(),
-        label({
-            label: {
-                name: config.get('name'),
-                pid: process.pid
-            },
-        }),
-        prettyPrint()
-    ),
-    dev: combine(
+/**
+ * Generate formatters base on config
+ * - 'dev' for development purpose
+ * - 'combined' for others
+ * @param {string} config
+ */
+function generateFormatters(config) {
+    if (config === 'combined')
+        return combine(
+            timestamp(),
+            label({
+                label: {
+                    name: config.get('name'),
+                    pid: process.pid
+                },
+            }),
+            prettyPrint()
+        );
+    return combine(
         splat(),
         simple()
-    ),
-};
+    );
+}
+
+/**
+ * Generate transports base on config
+ * @param {Array} arr
+ */
+function generateTransports(arr = []) {
+    return arr.map(({ type, level, filename }) => {
+        if (type === 'file') {
+            return new transports.File({
+                level,
+                filename: filename.replace('[timestamp]', Date.now().toString()),
+            });
+        } else {
+            return new transports.Console({
+                level,
+            });
+        }
+    });
+}
 
 const logger = createLogger({
-    format: loggerFormats[config.get('logger:format')],
+    format: generateFormatters(config.get('logger:format')),
     level: config.get('logger:level'),
-    transports: loggerTransports,
+    transports: generateTransports(config.get('logger:transports')),
 });
 
 
 exports.logger = logger;
-exports.LoggerStream = class LoggerStream {
+exports.LoggerStream = class InfoLoggerStream {
     write(data) {
         logger.info(data.replace('\n', ''));
     }
